f34c533524f6edd7211fb554839d1ad2e8991cbe
tomptrs <tom_ptrs@yahoo.com>
2025-10-20 17:20:39 +0200

    Commands

---
+++ Command/Command/Commands/HotTubCommand.cs
@@ -0,0 +1,25 @@
+﻿using Command.Interfaces;
+using Command.Vendor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Commands
+{
+    internal class HotTubCommand:ICommand
+    {
+        HotTub hotTub;
+        public HotTubCommand(HotTub hotTub)
+        {
+            this.hotTub = hotTub;
+        }
+
+        public void Execute()
+        {
+            hotTub.SetTemperature(40);
+            hotTub.JetsOn();
+        }
+    }
+}
---
+++ Command/Command/Commands/OutDoorLightCommand.cs
@@ -0,0 +1,23 @@
+﻿using Command.Interfaces;
+using Command.Vendor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command.Commands
+{
+    internal class OutDoorLightCommand : ICommand
+    {
+        OutDoorLight light;
+        public OutDoorLightCommand(OutDoorLight light)
+        {
+            this.light = light;
+        }
+        public void Execute()
+        {
+            light.On();
+        }
+    }
+}
